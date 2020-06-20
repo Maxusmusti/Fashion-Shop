b754bf02fc888331547d648c7d009896968336d6
Mustafa Eyceoz <maxusmusti@gmail.com>
2020-06-20 14:25:08 -0400

    Fix for setup.py

---
+++ flask_backend/modules/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='testo', version='1.0.0', py_modules=['testo'])
--- flask_backend/setup.py
+++
@@ -1,3 +0,0 @@
-from distutils.core import setup
-
-setup(name='db', version='1.0.0', py_modules=['db'])
