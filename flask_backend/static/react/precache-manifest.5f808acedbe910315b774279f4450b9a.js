51655fad40710e02bd81b3060cfefb2f938da01e
Mustafa Eyceoz <maxusmusti@gmail.com>
2020-06-20 14:36:07 -0400

    Added actual buiild to live

---
+++ flask_backend/static/react/precache-manifest.5f808acedbe910315b774279f4450b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6176de01e9c1b9f2c5e232e67c11bee5",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "f55ebd4375090285bdb3",
+    "url": "/static/react/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7aa9c687810fec8c45c6",
+    "url": "/static/react/js/2.4c67bb48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.4c67bb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55ebd4375090285bdb3",
+    "url": "/static/react/js/main.4b3d024c.chunk.js"
+  },
+  {
+    "revision": "8af987235234470a81eb",
+    "url": "/static/react/js/runtime-main.8e4365a4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/react/media/logo.ee7cd8ed.svg"
+  }
+]);
